7948023e46ba1062451b10976a801f0f808a64da
Sjors van Holst <sjorsvanholst@gmail.com>
2023-01-03 17:24:03 +0100

    💩 Attempting login

--- app.config.ts
+++ app.config.ts
@@ -2,6 +2,16 @@ import "dotenv/config";
 
 export default {
   expo: {
+    name: "Disposable",
+    slug: "Disposable",
+    version: "1.0.0",
+    android: {
+      package: "app.disposable_camera.disposable",
+    },
+    ios: {
+      bundleIdentifier: "app.disposable-camera.disposable",
+    },
+    scheme: "app.disposable_camera.disposable",
     extra: {
       apiDomain: process.env.API_DOMAIN,
       apiVersion: process.env.API_VERSION,
@@ -11,6 +21,9 @@ export default {
       firebaseKey: process.env.FIREBASE_KEY,
       firebaseDomain: process.env.FIREBASE_DOMAIN,
       firebaseProject: process.env.FIREBASE_PROJECT,
+      eas: {
+        projectId: "4ad482b5-f97d-4cc9-8642-86e507b6b81d",
+      },
     },
   },
 };
--- screen/ScreenLogin.tsx
+++ screen/ScreenLogin.tsx
@@ -1,45 +1,64 @@
-import React from "react";
-import { StyleSheet, Text, TextInput, View } from "react-native";
 import Icon from "react-native-vector-icons/FontAwesome";
-import { Button, Input } from "react-native-elements";
-import {
-  getAuth,
-  GoogleAuthProvider,
-  signInWithEmailAndPassword,
-  signInWithPopup,
-} from "firebase/auth";
+import auth from "../config/firebase";
+import React from "react";
+import { Platform } from "react-native";
+import { makeRedirectUri } from 'expo-auth-session';
 
-const auth = getAuth();
+// TODO: Use react-native button instead of react-native-elements
+import { Button } from "react-native-elements";
+import { useEffect } from "react";
+import { GoogleAuthProvider } from "firebase/auth/react-native";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { getAuth, signInWithCredential } from "firebase/auth";
+import { StyleSheet, Text, TextInput, View } from "react-native";
+
+import * as Google from "expo-auth-session/providers/google";
+import * as WebBrowser from "expo-web-browser";
+
+import * as AuthSession from 'expo-auth-session';
+
+WebBrowser.maybeCompleteAuthSession();
 
 const SignInScreen = () => {
-  const [value, setValue] = React.useState({
-    email: "",
-    password: "",
-    error: "",
+  const [request, response, promptAsync] = Google.useAuthRequest({
+    // iosClientId: "107127868067-6666bnbvlcd2fvvojdtncq1h3bihkfjb.apps.googleusercontent.com",
+    // webClientId: "107127868067-t96a83429ou61k26fijep98i3a0ut4ak.apps.googleusercontent.com",
+    // expoClientId: "107127868067-t96a83429ou61k26fijep98i3a0ut4ak.apps.googleusercontent.com",
+    androidClientId: "107127868067-t96a83429ou61k26fijep98i3a0ut4ak.apps.googleusercontent.com",
+    redirectUri: AuthSession.makeRedirectUri({ native: 'app.disposable_camera.disposable://' }),
   });
 
+  console.log("broh");
+
+  const [value, setValue] = React.useState({
+    email: "",
+    error: "",
+    password: "",
+  });
+
+  useEffect(() => {
+    if (response?.type === "success") {
+      const token = response.params.id_token;
+
+      // TODO: Remove GetAuth since we already import it
+      const auth = getAuth();
+      const credential = GoogleAuthProvider.credential(token);
+      signInWithCredential(auth, credential);
+    }
+  }, [response]);
+
   async function signIn() {
-    if (value.email === "" || value.password === "") {
-      setValue({
-        ...value,
-        error: "Email and password are mandatory.",
-      });
-      return;
-    }
+    // if (value.email === "" || value.password === "") {
+    //   setValue({ ...value, error: "Email and password are mandatory." });
+    //   return;
+    // }
 
-    try {
-      await signInWithEmailAndPassword(auth, value.email, value.password);
-    } catch (error) {
-      setValue({
-        ...value,
-        error: error.message,
-      });
-    }
-  }
-
-  async function signInWithGoogle() {
-    const provider = new GoogleAuthProvider();
-    await signInWithPopup(auth, provider);
+    // try {
+    //   await signInWithEmailAndPassword(auth, value.email, value.password);
+    // } catch (error) {
+    //   setValue({ ...value, error: error.message });
+    //   return;
+    // }
   }
 
   return (
@@ -55,13 +74,8 @@ const SignInScreen = () => {
       <View style={styles.controls}>
         <TextInput
           placeholder="Email"
-          // containerStyle={styles.control}
           value={value.email}
           onChangeText={(text) => setValue({ ...value, email: text })}
-          // leftIcon={<Icon
-          //   name='envelope'
-          //   size={16}
-          // />}
         />
 
         <TextInput
@@ -70,15 +84,16 @@ const SignInScreen = () => {
           value={value.password}
           onChangeText={(text) => setValue({ ...value, password: text })}
           secureTextEntry={true}
-          // leftIcon={<Icon
-          //   name='key'
-          //   size={16}
-          // />}
         />
 
         <Button title="Sign in" buttonStyle={styles.control} onPress={signIn} />
 
-        <Button title="Sign in with Google" onPress={signInWithGoogle}>
+        
+        <Button
+          title="Sign in with Google"
+          disabled={!request}
+          onPress={() => promptAsync()}
+        >
           <Icon name="google" size={16} />
         </Button>
       </View>
